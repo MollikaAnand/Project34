21001aabe24146eebff35b74722a6616eca9ab27
arfana017 <70667415+arfana017@users.noreply.github.com>
2020-12-02 17:39:17 -0800

    Update sketch.js

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -15,7 +15,11 @@ function setup() {
     bob4 = new Pendulum(windowWidth/2-450,windowHeight/2-800);
     bob5 = new Pendulum(windowWidth/2-540,windowHeight/2-800);
 
-    sling1 = new Sling(bob1.body,{x:windowWidth/2-190, y: windowHeight/2-200});
+    sling1 = new Sling(bob1.body,{x:windowWidth/2-640, y: windowHeight/2-500});
+    sling2 = new Sling(bob2.body,{x:windowWidth/2-730, y: windowHeight/2-500});
+    sling3 = new Sling(bob3.body,{x:windowWidth/2-820, y: windowHeight/2-500});
+    sling4 = new Sling(bob4.body,{x:windowWidth/2-910, y: windowHeight/2-500});
+    sling5 = new Sling(bob5.body,{x:windowWidth/2-1000, y: windowHeight/2-500});
 
 }
 
@@ -31,5 +35,15 @@ function draw() {
     bob5.display();
 
     sling1.display();
+    sling2.display();
+    sling3.display();
+    sling4.display();
+    sling5.display();
+
+}
+
+function mouseDragged() {
+
+        Matter.Body.setPosition(bob5.body, {x: mouseX , y: mouseY});
 
 }
