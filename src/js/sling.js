af796471df85526b1f0bd751de38eca4d541bc39
arfana017 <70667415+arfana017@users.noreply.github.com>
2020-12-02 17:40:02 -0800

    Update sling.js

--- src/js/sling.js
+++ src/js/sling.js
@@ -11,18 +11,28 @@ constructor(bodyA,pointB) {
         length: 220
 
     }
-//fixxxxxxxx add displayyyyy
+    
     this.bodyA = bodyA
     this.pointB = pointB;
     //this.pointX = bodyA.x;
     //this.pointY = bodyA.y - 250;
     this.sling = Constraint.create(options);
     World.add(world,this.sling);
-    push();
-    strokeWeight(3.5);
-    stroke("white");
-    line(pointpointB.x,pointB.y)
-    pop();
+   
+}
+
+display() {
+
+    if (this.sling.bodyA) {
+
+        var pointA = this.bodyA.position;
+	    var pointB = this.pointB
+
+        strokeWeight(2);
+        stroke("white");		
+		line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+    }
 
 }
 
